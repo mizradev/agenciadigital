7d5a83125e7f413491fd3cf53c91cfa134f32354
soymizra <jorgemizrain@gmail.com>
2021-02-21 14:53:03 -0600

    Se agrego el menu en version movil

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,9 +4,22 @@
       var currentScrollPos = window.pageYOffset;
       if (prevScrollpos > currentScrollPos) {
         document.getElementById("nav").style.top = "0";
+        console.log('arriba');
       } else {
         document.getElementById("nav").style.top = "-128px";
+        
       }
       prevScrollpos = currentScrollPos;
-    } 
+    }
+
+
+    function test(){
+      console.log('Hola');
+    }
 })();
+
+// menu hamburger
+    function menuHamburger(x){
+      x.classList.toggle("change");
+      document.querySelector('nav').classList.toggle('opacity-0');
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+    	colors:{
+    		dark: "#001233",
+		    dark2: '#001845',
+		    primary: '#0466C8',
+		    success: '#42E2B8',
+		    danger: '#214E34', 
+		    secondary: '#FFFAFF',
+		    warning: '#EEE0CB'
+    	}
+    },
   },
   variants: {
     extend: {},
